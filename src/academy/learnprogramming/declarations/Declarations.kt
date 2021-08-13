9dff4c4f8502023d419cd6a76a915e526e80cac5
Lucas Horta <lucashorta@gmail.com>
2021-08-12 21:28:38 -0300

    finalizando seção 3

--- src/academy/learnprogramming/declarations/Declarations.kt
+++ src/academy/learnprogramming/declarations/Declarations.kt
@@ -3,28 +3,38 @@ package academy.learnprogramming.declarations
 typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
+
     var number: Int
     number = 10
     number = 20
 
-    val names = arrayListOf("João", "Joana", "Maria")
-    println(names[1])
-
-    val employees: EmployeeSet
 
     val employee1 = Employee(name = "Lucas", id = 100)
     employee1.name = "Lucas Horta"
 
-    val employee2: Employee
-    val number2 = 100
+    println(employee1)
 
-    if (number < number2) {
-        employee2 = Employee(name = "John", id = 200)
-    } else {
-        employee2 = Employee(name = "Jack", id = 300)
-    }
+    val troco = 4.25
 
+    println("Seu troco é R$$troco")
 
+    val numerador = 10.99
+    val denominador = 20.0
+
+    println("O valor de $numerador dividido por $denominador é ${numerador/denominador}")
+
+    println("Os id do empregado ${employee1.name} é ${employee1.id}")
+
+    val egg = "Humpty"
+    val cantiga = """$egg Dumpty sat on the wall
+                    |$egg Dumpty had a great fall
+                    |All the king's horses and all the king's men
+                    |Couldn't put $egg together again.""".trimMargin()
+    println(cantiga)
 }
 
-class Employee(var name: String, val id: Int)
+class Employee(var name: String, val id: Int) {
+    override fun toString(): String {
+        return "Employee(name='$name', id=$id)"
+    }
+}
